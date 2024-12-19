3428caa63bfd2101cf2cebadd6cd58852308d7c6
abhishek-kiet <abhishek.2428cse1292@kiet.edu>
2024-12-19 22:56:44 +0530

    Function and Object

---
+++ start.js
@@ -0,0 +1,52 @@
+//! Function Make 
+
+function show(){
+    document.getElementById('block-1').style.display='none';
+}
+
+
+//! function return 
+
+function returnFunction(x,y){
+    return x * y ;
+}
+
+//!  Function to calculate Celcius To Fahrenheit 
+
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+  }
+  
+  let value = toCelsius();
+
+
+//!  In JavaScript, almost "everything" is an object.
+//!
+//!  Objects are objects
+//!  Maths are objects
+//!  Functions are objects
+//!  Dates are objects
+//!  Arrays are objects
+//!  Maps are objects
+//!  Sets are objects
+//!  All JavaScript values, except primitives, are objects.
+
+// Create an Object
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age:50,
+    eyeColor: "blue"
+  };
+  
+  // Create a Copy
+  const x = person;
+  
+  // Change Age
+  x.age = 10;
+  
+  document.getElementById("demo").innerHTML =
+  person.firstName + " is " + person.age + " years old.";
+  //! here If change happen in Copied object then it will effect on main object 
+  
+  
